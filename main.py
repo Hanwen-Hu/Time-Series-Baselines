2c551dd11e5011762669c7e30f5630097ebeb517
Hanwen-Hu <hanwen_hu@126.com>
2024-01-12 00:27:39 +0800

    LTSF

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ dims = {'Electricity': 370, 'ETTh': 14, 'ETTm': 14, 'Exchange': 8, 'QPS': 10, 'S
 seq_lens = {'Electricity': 384, 'ETTh': 96, 'ETTm': 384, 'Exchange': 120,'QPS': 240, 'Solar': 1152, 'Traffic': 96, 'Weather': 576}
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Electricity', help='Dataset Name')
-parser.add_argument('--model', type=str, default='DLinear', help='DLinear, NLinear, PatchTST')
-parser.add_argument('--pred_len', type=int, default=720, help='length of output sequence')
+parser.add_argument('-dataset', type=str, default='Electricity', help='Dataset Name')
+parser.add_argument('-model', type=str, default='DLinear', help='DLinear, NLinear, PatchTST')
+parser.add_argument('-pred_len', type=int, default=720, help='length of output sequence')
 
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=3e-4, help='learning rate')
-parser.add_argument('--epochs', type=int, default=100)
-parser.add_argument('--patience', type=int, default=5, help='patience')
+parser.add_argument('-batch_size', type=int, default=128, help='batch size')
+parser.add_argument('-learning_rate', type=float, default=3e-4, help='learning rate')
+parser.add_argument('-epochs', type=int, default=100)
+parser.add_argument('-patience', type=int, default=5, help='patience')
 
 args = parser.parse_args()
 args.dim = dims[args.dataset]
